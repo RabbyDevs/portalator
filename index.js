e189090a782030ead71e7af53e950f9fca90c307
RabbyDevs <mohammedaly2312@gmail.com>
2023-04-21 05:18:17 +0300

    this is exciting

---
+++ index.js
@@ -0,0 +1,22 @@
+const resourceTable = {
+	'Aluminum': 0,
+	'Chromium': 0,
+	'Copper': 0,
+	'Diamond': 0,
+	'Gold': 0,
+	'Iron': 0,
+	'Oil': 0,
+	'Phosphate': 0,
+	'Titanium': 0,
+	'Tungsten': 0,
+	'Uranium': 0,
+	'Electronics': 0,
+	'Fertilizer': 0,
+	'Motor Parts': 0,
+	'Consumers': 0,
+	'Steel': 0,
+	'Aircraft Parts': 0,
+};
+
+resourceTable.Aluminum = 10;
+console.log(resourceTable);
