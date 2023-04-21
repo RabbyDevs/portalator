208a3aa965499146bdcb06df757e05d57ebd0220
RabbyDevs <mohammedaly2312@gmail.com>
2023-04-22 00:30:46 +0300

    woo beta.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # portalator
 Portalator is a portable version of the RON calculator project but written in Node, hence the name "portalator".
+
+# Installation
+You will need the things below:
+* [Node.js 19](https://nodejs.org/en/download)
+* [Git](https://git-scm.com/downloads)
+First, open a terminal window then do this command:
+```js
+git clone https://github.com/RabbyDevs/portalator
+```
+Second, cd into the portalator folder with this command:
+```js
+cd portalator
+```
+Finally, launch the program:
+```js
+node index.js
+```
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const prompt = require('prompt-sync')();
-
-const resourceTable = {
-	'Aluminum': 0,
-	'Chromium': 0,
-	'Copper': 0,
-	'Diamond': 0,
-	'Gold': 0,
-	'Iron': 0,
-	'Oil': 0,
-	'Phosphate': 0,
-	'Titanium': 0,
-	'Tungsten': 0,
-	'Uranium': 0,
-	'Electronics': 0,
-	'Fertilizer': 0,
-	'Motor Parts': 0,
-	'Consumers': 0,
-	'Steel': 0,
-	'Aircraft Parts': 0,
-};
+const Decimal = require('decimal.js');
 
 const modifier = prompt('whats ur factory output %? ');
 const cities = prompt('city count? ');
-const consumerAmount = Math.floor((40 + modifier)) * cities;
+const consumerAmount = Math.floor((40 * (1 + modifier / 100)));
+const consumerFactories = Math.ceil((consumerAmount * cities) / consumerAmount);
 
-resourceTable.Consumers = consumerAmount;
+const electronicsAmount = consumerFactories * 3;
+const electronicsFactories = Math.ceil(electronicsAmount / (16 * (1 + modifier / 100)));
+const goldAndCopper = electronicsFactories * 2;
 
-// ok ill do this stuff later im tired
+const fertilizerAmount = consumerFactories * 2.5;
+const fertilizerFactories = Math.ceil(fertilizerAmount / (12 * (1 + (modifier / 100))));
+const phosphate = fertilizerFactories * 3.5;
+
+const motorsAmount = consumerFactories * 2.5;
+const motorsFactories = Math.ceil(motorsAmount / (6 * (1 + (modifier / 100))));
+const tungsten = motorsFactories * 2;
+
+const steelAmount = motorsFactories;
+const steelFactories = Math.ceil(steelAmount / (3 * (1 + (modifier / 100))));
+const iron = steelFactories * 4;
+const titanium = Decimal.mul(steelFactories, 0.2);
+
+console.log(`Electronic Factories: ${electronicsFactories}\n	Gold: ${goldAndCopper}\n	Copper: ${goldAndCopper}`);
+console.log(`Fertilizer Factories: ${fertilizerFactories}\n	Phosphate: ${phosphate}`);
+console.log(`Motor Factories: ${motorsFactories}\n	Steel: ${steelAmount}\n	Tungsten: ${tungsten}`);
+console.log(`Steel Factories: ${steelFactories}\n	Iron: ${iron}\n	Titanium: ${titanium}`);
